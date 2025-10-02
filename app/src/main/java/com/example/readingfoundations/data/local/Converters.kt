1449ef2e4eb8eb9fa84a04edfefee19f428db66b
ColbyCabrera <gdthyispro@gmail.com>
2025-10-02 18:05:26 -0400

    Refactor: Move Gson instance to companion object
    Moves the `gson` and `type` properties inside the `Converters` class to a companion object. This ensures that a single instance of these properties is shared across all instances of the `Converters` class, making it a more efficient use of resources.

--- app/src/main/java/com/example/readingfoundations/data/local/Converters.kt
+++ app/src/main/java/com/example/readingfoundations/data/local/Converters.kt
@@ -1,10 +1,10 @@
 package com.example.readingfoundations.data.local
 
 import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class Converters {
-    private val gson = com.google.gson.Gson()
-    private val type = object : com.google.gson.reflect.TypeToken<List<String>>() {}.type
     @TypeConverter
     fun fromString(value: String?): List<String>? {
         return value?.let { gson.fromJson(it, type) }
@@ -12,5 +12,10 @@ class Converters {
     @TypeConverter
     fun fromList(list: List<String>?): String? {
         return list?.let { gson.toJson(it, type) }
+    }
+
+    companion object {
+        private val gson = Gson()
+        private val type = object : TypeToken<List<String>>() {}.type
     }
 }
