0a410d06185519267e5a644f660951d931ea4912
ColbyCabrera <gdthyispro@gmail.com>
2025-10-01 22:04:20 -0400

    feat: Add Material Icons dependency
    This commit adds the `androidx.compose.material:material-icons-extended` dependency to the project. This will allow the use of the extended set of Material Design icons within the application's Composable functions.
    
    The Room library version was also updated from `2.6.1` to `2.8.1`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.google.material)
 
     // Room Database
     implementation(libs.bundles.room)
